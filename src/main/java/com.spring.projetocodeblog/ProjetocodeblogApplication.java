63ed5e14c4d01da6ebf847124d276580ffe21ae4
mgracioli <michel-rg@hotmail.com>
2021-06-25 10:35:09 -0300

    primeiro commit

---
+++ src/main/java/com.spring.projetocodeblog/ProjetocodeblogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.projetocodeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocodeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocodeblogApplication.class, args);
+	}
+
+}
